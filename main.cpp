cdd097b86ea05e05f1e1a64460e3a668e0692ed6
Laurentiu Galis <75796584+Laurentiuu@users.noreply.github.com>
2021-03-25 22:54:51 +0200

    src

---
+++ main.cpp
@@ -0,0 +1,398 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp> //core glm functionality
+#include <glm/gtc/matrix_transform.hpp> //glm extension for generating common transformation matrices
+#include <glm/gtc/matrix_inverse.hpp> //glm extension for computing inverse matrices
+#include <glm/gtc/type_ptr.hpp> //glm extension for accessing the internal data structure of glm types
+
+#include "Window.h"
+#include "Shader.hpp"
+#include "Camera.hpp"
+#include "Model3D.hpp"
+
+#include <iostream>
+
+// window
+gps::Window myWindow;
+
+// matrices
+glm::mat4 model;
+glm::mat4 view;
+glm::mat4 projection;
+glm::mat3 normalMatrix;
+
+// light parameters
+glm::vec3 lightDir;
+glm::vec3 lightColor;
+
+// shader uniform locations
+GLuint modelLoc;
+GLuint viewLoc;
+GLuint projectionLoc;
+GLuint normalMatrixLoc;
+GLuint lightDirLoc;
+GLuint lightColorLoc;
+
+// camera
+gps::Camera myCamera(
+    glm::vec3(0.0f, 0.0f, 3.0f),
+    glm::vec3(0.0f, 0.0f, -10.0f),
+    glm::vec3(0.0f, 1.0f, 0.0f));
+
+GLfloat cameraSpeed = 0.1f;
+
+GLboolean pressedKeys[1024];
+
+// models
+gps::Model3D myModel;
+gps::Model3D lightCube;
+gps::Model3D moara;
+//unghiul de rotire
+GLfloat angle;
+
+//cubul de lumina
+float angleY = 0.0f;
+GLfloat lightAngle;
+
+// shaders
+gps::Shader myBasicShader;
+gps::Shader lightShader;
+//gps::Shader modelShader;
+
+//camera
+int glWindowWidth = 1024;
+int glWindowHeight = 768;
+float mouseSpeed;
+float pitch;
+float yaw;
+bool firstMouse = true;
+
+
+
+GLenum glCheckError_(const char *file, int line)
+{
+	GLenum errorCode;
+	while ((errorCode = glGetError()) != GL_NO_ERROR) {
+		std::string error;
+		switch (errorCode) {
+            case GL_INVALID_ENUM:
+                error = "INVALID_ENUM";
+                break;
+            case GL_INVALID_VALUE:
+                error = "INVALID_VALUE";
+                break;
+            case GL_INVALID_OPERATION:
+                error = "INVALID_OPERATION";
+                break;
+            case GL_STACK_OVERFLOW:
+                error = "STACK_OVERFLOW";
+                break;
+            case GL_STACK_UNDERFLOW:
+                error = "STACK_UNDERFLOW";
+                break;
+            case GL_OUT_OF_MEMORY:
+                error = "OUT_OF_MEMORY";
+                break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION:
+                error = "INVALID_FRAMEBUFFER_OPERATION";
+                break;
+        }
+		std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+	}
+	return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+void windowResizeCallback(GLFWwindow* window, int width, int height) {
+	fprintf(stdout, "Window resized! New width: %d , and height: %d\n", width, height);
+	//TODO
+
+}
+
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+	if (key >= 0 && key < 1024) {
+        if (action == GLFW_PRESS) {
+            pressedKeys[key] = true;
+        } else if (action == GLFW_RELEASE) {
+            pressedKeys[key] = false;
+        }
+    }
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+	//setez cursorul invizibil si il pun in mijlocul ecranului
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	int lastX = glWindowWidth / 2;
+	int lastY = glWindowHeight / 2;
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	mouseSpeed = 0.0005f;
+	yaw += mouseSpeed * float(lastX - xpos);
+	pitch += mouseSpeed * float(lastY - ypos);
+
+	myCamera.rotate(pitch, yaw);
+	glfwSetCursorPos(window, lastX, lastY);
+
+	myCamera.rotate(pitch,yaw);
+	//update view matrix
+	view = myCamera.getViewMatrix();
+	myBasicShader.useShaderProgram();
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	// compute normal matrix for teapot
+	normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+}
+
+void processMovement() {
+	if (pressedKeys[GLFW_KEY_W]) {
+		myCamera.move(gps::MOVE_FORWARD, cameraSpeed);
+		//update view matrix
+        view = myCamera.getViewMatrix();
+        myBasicShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
+	}
+
+	if (pressedKeys[GLFW_KEY_S]) {
+		myCamera.move(gps::MOVE_BACKWARD, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myBasicShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
+	}
+
+	if (pressedKeys[GLFW_KEY_A]) {
+		myCamera.move(gps::MOVE_LEFT, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myBasicShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
+	}
+
+	if (pressedKeys[GLFW_KEY_D]) {
+		myCamera.move(gps::MOVE_RIGHT, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myBasicShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
+	}
+
+    if (pressedKeys[GLFW_KEY_Q]) {
+        angle -= 1.0f;
+        // update model matrix for teapot
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
+        // update normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
+    }
+
+    if (pressedKeys[GLFW_KEY_E]) {
+        angle += 1.0f;
+        // update model matrix for teapot
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
+        // update normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
+    }
+
+	//wireframe
+	if (pressedKeys[GLFW_KEY_1]) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	//poligonala
+	if (pressedKeys[GLFW_KEY_2]) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+	}
+	//smooth
+	if (pressedKeys[GLFW_KEY_3]) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+
+	if (pressedKeys[GLFW_KEY_K]) {
+
+		lightAngle += 0.7f;
+		if (lightAngle > 360.0f)
+			lightAngle -= 360.0f;
+		glm::vec3 lightDirTr = glm::vec3(glm::rotate(glm::mat4(1.0f), glm::radians(lightAngle), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(lightDir, 1.0f));
+		myBasicShader.useShaderProgram();
+		glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDirTr));
+	}
+
+	if (pressedKeys[GLFW_KEY_L]) {
+		lightAngle -= 0.7f;
+		if (lightAngle < 0.0f)
+			lightAngle += 360.0f;
+		glm::vec3 lightDirTr = glm::vec3(glm::rotate(glm::mat4(1.0f), glm::radians(lightAngle), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(lightDir, 1.0f));
+		myBasicShader.useShaderProgram();
+		glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDirTr));
+	}
+
+	/*
+	if (pressedKeys[GLFW_KEY_Z]) {
+
+		PlaySound(TEXT("sound.wav"), NULL, SND_ASYNC);
+	}
+
+	if (pressedKeys[GLFW_KEY_X]) {
+
+		PlaySound(NULL, 0, 0);
+	}
+	*/
+}
+
+void initOpenGLWindow() {
+    myWindow.Create(1024, 768, "OpenGL Project Core");
+}
+
+void setWindowCallbacks() {
+	glfwSetWindowSizeCallback(myWindow.getWindow(), windowResizeCallback);
+    glfwSetKeyCallback(myWindow.getWindow(), keyboardCallback);
+    glfwSetCursorPosCallback(myWindow.getWindow(), mouseCallback);
+}
+
+void initOpenGLState() {
+	glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
+	glViewport(0, 0, myWindow.getWindowDimensions().width, myWindow.getWindowDimensions().height);
+    glEnable(GL_FRAMEBUFFER_SRGB);
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+	glEnable(GL_CULL_FACE); // cull face
+	glCullFace(GL_BACK); // cull back face
+	glFrontFace(GL_CCW); // GL_CCW for counter clock-wise
+}
+
+void initModels() {
+	myModel.LoadModel("models/scena.obj");
+	moara.LoadModel("models/moara de vant/windmill.obj");
+	lightCube.LoadModel("models/cube/cube.obj");
+}
+
+void initShaders() {	
+	myBasicShader.loadShader("shaders/basic.vert","shaders/basic.frag");
+	lightShader.loadShader("shaders/lightCube.vert", "shaders/lightCube.frag");
+	//modelShader.loadShader("shaders/model.vert", "shaders/model.frag");
+}
+
+void initUniforms() {
+	myBasicShader.useShaderProgram();
+
+    // create model matrix for teapot
+    model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+	modelLoc = glGetUniformLocation(myBasicShader.shaderProgram, "model");
+
+	// get view matrix for current camera
+	view = myCamera.getViewMatrix();
+	viewLoc = glGetUniformLocation(myBasicShader.shaderProgram, "view");
+	// send view matrix to shader
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+    // compute normal matrix for scene
+    normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
+	normalMatrixLoc = glGetUniformLocation(myBasicShader.shaderProgram, "normalMatrix");
+
+	// create projection matrix
+	projection = glm::perspective(glm::radians(45.0f),
+                               (float)myWindow.getWindowDimensions().width / (float)myWindow.getWindowDimensions().height,
+                               0.1f, 300.0f);
+	projectionLoc = glGetUniformLocation(myBasicShader.shaderProgram, "projection");
+	// send projection matrix to shader
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));	
+
+	//set the light direction (direction towards the light)
+	lightDir = glm::vec3(0.0f, 1.0f, 1.0f);
+	lightDirLoc = glGetUniformLocation(myBasicShader.shaderProgram, "lightDir");
+	// send light dir to shader
+	glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDir));
+
+	//set light color
+	lightColor = glm::vec3(0.467f, 0.533f, 0.6f); //am modificat putin culoarea
+	lightColorLoc = glGetUniformLocation(myBasicShader.shaderProgram, "lightColor");
+	// send light color to shader
+	glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
+}
+
+
+void renderScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//render the scene
+
+		// select active shader program
+	myBasicShader.useShaderProgram();
+
+	//send teapot model matrix data to shader
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	//send teapot normal matrix data to shader
+	glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+	// draw teapot
+	myModel.Draw(myBasicShader);
+
+
+	//cealalta sursa de lumina
+	model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+	glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+	//lightCube.Draw(myBasicShader);
+
+}
+
+void cleanup() {
+    myWindow.Delete();
+    //cleanup code for your own data
+}
+
+int main(int argc, const char * argv[]) {
+
+    try {
+        initOpenGLWindow();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+	
+	glPushMatrix();
+	
+
+    initOpenGLState();
+	initModels();
+	initShaders();
+	initUniforms();
+    setWindowCallbacks();
+
+
+	glCheckError();
+	// application loop
+	while (!glfwWindowShouldClose(myWindow.getWindow())) {
+        processMovement();
+	    renderScene();
+
+		glfwPollEvents();
+		glfwSwapBuffers(myWindow.getWindow());
+
+		glCheckError();
+	}
+
+	cleanup();
+
+    return EXIT_SUCCESS;
+}
